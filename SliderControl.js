ada613c100c795bd41951a366c5496b239210815
Will Fisher <will@teknofire.net>
2014-06-26 17:29:57 -0800

    refactoring to make more friendly
    I’ve pulled out duplicated code into their own methods (buildTimestamp,
    updateTimestamp, updateLayer)
    Cleaned up scoping in the startSlider method to not require as many
    local variables
    timestamp formatting is handled in the method that uses them since they
    know what they need best (i.e. differences between timestamps display
    and wms timestamp variable)
    UI elements now use css classes instead of the hardcoded id attributes,
    this is more friendly to users that might want to have multiple sliders
    and for applying custom styling.
    Assumes now that time steps are in seconds and converts to microseconds
    (who wants to type all those zero’s anyway)
    Range option now only affects the slider, if you say range: false a
    range of dates is still provided to the wms server based on the
    timestep.  This is a much nicer since we don’t know the exact dates for
    the different time intervals in the wms.

--- SliderControl.js
+++ SliderControl.js
@@ -1,150 +1,148 @@
 L.Control.SliderControl = L.Control.extend({
-    options: {
-        position: 'topright',
-        layers: null,
-        maxValue: -1,
-        minValue: -1,
-	startTime: -1,
-	endTime: -1,
-	timeStep: 5000,
-        range: false
-    },
+  options: {
+    position: 'topright',
+    layers: null,
+    maxValue: -1,
+    minValue: -1,
+    startTime: -1,
+    endTime: -1,
+    timeStep: 3600,
+    range: false
+  },
 
-    initialize: function (options) {
-        L.Util.setOptions(this, options);
+  initialize: function(options) {
+    L.Util.setOptions(this, options);
 
-	// Initialize variables
-	this._layer = null;
-	this.multilayer = false;
+    // Initialize variables
+    this._layer = null;
+    this.multilayer = false;
 
-	// Check for multiple layers passed to SliderControl
-	if (this.options.layers == null) { 
-        	this._layer = this.options.layer;
-		this.multilayer = false;
-	} else {
-		this._layer = this.options.layers;
-		this.multilayer = true;
-	}
-    },
-
-    setPosition: function (position) {
-        var map = this._map;
-
-        if (map) {
-            map.removeControl(this);
-        }
-
-        this.options.position = position;
-
-        if (map) {
-            map.addControl(this);
-        }
-        this.startSlider();
-        return this;
-    },
-
-    onAdd: function (map) {
-        this.options.map = map;
-
-        // Create a control sliderContainer with a jquery ui slider
-        var sliderContainer = L.DomUtil.create('div', 'slider', this._container);
-        $(sliderContainer).append('<div id="leaflet-slider" style="width:200px"><div class="ui-slider-handle"></div><div id="slider-timestamp" style="width:200px; border-top: 1px solid black; margin-top:10px;background-color:#FFFFFF"></div></div>');
-        // Prevent map panning/zooming while using the slider
-        $(sliderContainer).mousedown(function () {
-            map.dragging.disable();
-        });
-        $(document).mouseup(function () {
-            map.dragging.enable();
-        });
-
-        var options = this.options;
-
-        // Make sure a layer has been passed before creating a slider
-        if (!this._layer) {
-            console.log("Error: You have to specify a layer via new SliderControl({layer: your_layer}); or like SliderControl({layers: [your_layer1, your_layer2]});");
-        }
-        return sliderContainer;
-    },
-
-    onRemove: function (map) {
-        map.removeLayer(this._layer);
-        $('#leaflet-slider').remove();
-    },
-
-    startSlider: function () {
-        options = this.options;
-	var curLayer  = this._layer;
-
-	// Current position of the slider  
-	var curTime = new Date(options.startTime);
-	if (options.range) {
-	   // If a range, this is the second position for the slider
-	   var curTime2 = new Date(options.startTime);
-	}
-	
-	// Start and ending times are split into a number of slider positions based on number of milliseconds
-	// requested to separate each position
-	var beginTime = new Date(options.startTime);
-	var finalTime = new Date(options.endTime);
-	var timesteps = Math.round((finalTime - beginTime) / options.timeStep);
-
-	// Check for multiple layers passed to Slider Control
-	var multilayer = this.multilayer;
-
-        $("#leaflet-slider").slider({
-            range: options.range,
-	    min: 0,
-	    max: timesteps,
-            step: 1,
-	    slide: function (e, ui) {
-		var map = options.map;
-
-		if (options.range) {
-			curTime.setTime(beginTime.getTime() + (ui.values[0] * options.timeStep));
-			curTime2.setTime(beginTime.getTime() + (ui.values[1] * options.timeStep));
-			var timeString = curTime.toISOString() + " - " + curTime2.toISOString();
-                        $('#slider-timestamp').html(timeString);
-		} else {
-			curTime.setTime(beginTime.getTime() + (ui.value * options.timeStep));
-			var timeString = curTime.toISOString();
-                        $('#slider-timestamp').html(timeString);
-	        }
-            },
-            stop: function (e, ui) {
-                var map = options.map;
-
-		if (options.range) {
-			curTime.setTime(beginTime.getTime() + (ui.values[0] * options.timeStep));
-			curTime2.setTime(beginTime.getTime() + (ui.values[1] * options.timeStep));
-			var timeString = curTime.toISOString() + "/" + curTime2.toISOString();
-
-			// Leaflet's TileLayer's setParams function re-requests tiles based upon the new parameters
-			if (multilayer == true) {
-				for (var i = 0; i < curLayer.length; i++) {
-					curLayer[i].setParams({time: timeString});
-				}
-			} else {
-				curLayer.setParams({time: timeString});
-			}
-		} else {
-			curTime.setTime(beginTime.getTime() + (ui.value * options.timeStep));
-			var timeString = curTime.toISOString();
-
-			// Leaflet's TileLayer's setParams function re-requests tiles based upon the new parameters
-			if (multilayer == true) {
-				for (var i = 0; i < curLayer.length; i++) {
-					curLayer[i].setParams({time: timeString});
-				}
-			} else {
-				curLayer.setParams({time: timeString});
-			}
-		}
-            }
-	});
-	$('#slider-timestamp').html(curTime.toISOString());
+    // Start and ending times are split into a number of slider positions based on number of milliseconds
+    // requested to separate each position
+    this._begin_time = new Date(options.startTime);
+    this._final_time = new Date(options.endTime);
+    
+    // assume timeStep is in seconds and turn into microseconds
+    this.options.timeStep = this.options.timeStep * 1000;
+    
+    // Check for multiple layers passed to SliderControl
+    if (this.options.layers == null) {
+      this._layer = this.options.layer;
+      this.multilayer = false;
+    } else {
+      this._layer = this.options.layers;
+      this.multilayer = true;
     }
+  },
+
+  setPosition: function(position) {
+    var map = this._map;
+
+    if (map) {
+      map.removeControl(this);
+    }
+
+    this.options.position = position;
+
+    if (map) {
+      map.addControl(this);
+    }
+    this.startSlider();
+    return this;
+  },
+  
+  createSliderUI: function() {
+    // Create a control sliderContainer with a jquery ui slider
+    var sliderContainer = L.DomUtil.create('div', 'ui-slider-container', this._container);
+    
+    this._slider = $('<div class="ui-slider"><div class="ui-slider-handle"></div></div>');
+    $(sliderContainer).append(this._slider);
+    this._sliderTimestamp = $('<div class="ui-slider-timestamp"></div>');
+    $(sliderContainer).append(this._sliderTimestamp);
+    
+    return sliderContainer;
+  },
+
+  onAdd: function(map) {
+    this.options.map = map;
+
+    var sliderContainer = this.createSliderUI();
+    // Prevent map panning/zooming while using the slider
+    $(sliderContainer).mousedown(function() {
+      map.dragging.disable();
+    });
+    $(document).mouseup(function() {
+      map.dragging.enable();
+    });
+    
+    // Make sure a layer has been passed before creating a slider
+    if (!this._layer) {
+      console.log("Error: You must specify a layer via new SliderControl({layer: your_layer}); or like SliderControl({layers: [your_layer1, your_layer2]});");
+    }
+    return sliderContainer;
+  },
+
+  onRemove: function(map) {
+    map.removeLayer(this._layer);
+    $(this._slider).remove();
+  },
+
+  updateLayer: function(timestamps) {
+    timestamp = timestamps[0] + '/' + timestamps[1];
+    
+    if (this.multilayer == true) {
+      for (var i = 0; i < this._layer.length; i++) {
+        this._layer[i].setParams({
+          time: timestamp
+        });
+      }
+    } else {
+      this._layer.setParams({
+        time: timestamp
+      });
+    }
+  },
+  
+  updateTimestamp: function(timestamps) {
+    $(this._sliderTimestamp).html(timestamps[0] + ' - ' + timestamps[1]);
+  },
+  
+  buildTimestamp: function(start, end) {
+    this._timerange1 = new Date(this._begin_time.getTime() + (start * this.options.timeStep));
+    this._timerange2 = new Date(this._begin_time.getTime() + (end * this.options.timeStep));
+    
+    return [this._timerange1.toISOString(), this._timerange2.toISOString()];
+  },
+
+  startSlider: function() {
+    var me = this;
+    var timesteps = Math.round((me._final_time - me._begin_time) / me.options.timeStep);
+    
+    $(me._slider).slider({
+      range: me.options.range,
+      min: 0,
+      max: timesteps,
+      step: 1,
+      slide: function(e, ui) {
+        if (me.options.range) {
+          me.updateTimestamp(me.buildTimestamp(ui.values[0], ui.values[1]));
+        } else {
+          me.updateTimestamp(me.buildTimestamp(ui.value, ui.value+1));
+        }
+      },
+      stop: function(e, ui) {
+        if (me.options.range) {
+          me.updateLayer(me.buildTimestamp(ui.values[0], ui.values[1]));
+        } else {
+          me.updateLayer(me.buildTimestamp(ui.value, ui.value+1));
+        }
+      }
+    });
+    me.updateTimestamp(me.buildTimestamp(0,1));
+    me.updateLayer(me.buildTimestamp(0,1));
+  }
 });
 
-L.control.sliderControl = function (options) {
-    return new L.Control.SliderControl(options);
+L.control.sliderControl = function(options) {
+  return new L.Control.SliderControl(options);
 };
